f0a886de35dbed20e6cfa0fd8c44f7c52472a294
RobWitzlib <rob.witzlib@gmail.com>
2024-07-07 15:34:07 -0500

    fix vars

--- tf/tagging.tf
+++ tf/tagging.tf
@@ -1,5 +1,6 @@
 locals {
   default_tags = {
+    Team        = var.team
     Environment = var.environment
     Service     = var.service_name
     Repo        = "https://github.com/rwitzlib/stockmountain-marketviewer"
